8db9645321d5653c0cefc2c6872236d304e9123b
jotapixma <jorgeteran22@gmail.com>
2025-04-16 18:25:21 -0400

    babel config

--- src/app/page.js
+++ src/app/page.js
@@ -1,5 +1,5 @@
 'use client'
-import PopUpSearcher from "@/components/PopUpSearcher/PopUpSearcher";
+import PopUpSearcher from "@/src/components/PopUpSearcher/PopUpSearcher";
 import styles from "./page.module.css";
 
 export default function Home() {
