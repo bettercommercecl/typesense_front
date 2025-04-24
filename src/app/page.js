c5d239d2787e49eb8103dd8507e0a91b7e6d28a9
jotapixma <jorgeteran22@gmail.com>
2025-04-24 17:30:00 -0400

    sweet change to web components

--- src/app/page.js
+++ src/app/page.js
@@ -1,12 +1,24 @@
-'use client'
-import PopUpSearcher from "@/src/components/PopUpSearcher/PopUpSearcher";
-import styles from "./page.module.css";
+'use client';
+
+import React, { useEffect } from 'react';
+import styles from './page.module.css';
 
 export default function Home() {
+  useEffect(() => {
+    const script = document.createElement('script');
+    script.type = 'module';
+    script.src = 'https://typesense-front.vercel.app/components/PopUpSearcher/PopUpSearcher.js'; // Asegúrate de que esta URL sea correcta
+    document.body.appendChild(script);
+
+    return () => {
+        document.body.removeChild(script);
+    };
+  }, []);
+
   return (
     <div className={styles.page}>
       <main className={styles.main}>
-        <PopUpSearcher/>
+        <pop-up-searcher></pop-up-searcher> {/* Usar el Web Component */}
       </main>
     </div>
   );
