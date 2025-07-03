0e11c3963009df29b6f63f2b584f8fe56d8facc6
jotapixma <jorgeteran22@gmail.com>
2025-07-03 12:16:06 -0400

    update with input debounce

--- src/components/InputSearch/InputSearch.jsx
+++ src/components/InputSearch/InputSearch.jsx
@@ -1,9 +1,8 @@
 'use client';
 
-import React, { forwardRef, useEffect } from 'react';
-import getRequest from '../../api/getRequest';
+import React, { forwardRef } from 'react';
 import styles from './InputSearch.module.css';
-import { useRouter } from 'next/router';
+import useInputSearch from './useInputSearch';
 
 const InputSearch = forwardRef(({
 	typesenseCollection,
@@ -17,95 +16,25 @@ const InputSearch = forwardRef(({
 	onInputChange,
 	dropdownRef,
 }, ref) => {
-	const router = useRouter();
 
-	// search products in the getRequest endpoint
-	const handleSearch = async (e) => {
-		const query = e.target.value;
-		setSearchQuery(query);
-
-		if (query) {
-			setLoading(true);
-			try {
-				const results = await getRequest({query,typesenseCollection});
-				setResults(results);
-			} catch (error) {
-				console.error(error);
-				setResults([]);
-			}
-			setLoading(false);
-			document.querySelector(`.${styles.searchInput}`).classList.add(styles.inputExpanded);
-		} else {
-			setLoading(false);
-			// setResults([]);
-			document.querySelector(`.${styles.searchInput}`).classList.remove(styles.inputExpanded);
-		}
-
-		if (onInputChange) {
-			onInputChange(query);
-		}
-	};
-
-	// Close the input when the user clicks the close button
-	const handleCloseSearch = (event) => {
-		event.stopPropagation();
-		setExpandSearch(false);
-		setSearchQuery('');
-		setResults([]);
-		if (ref && ref.current) {
-			ref.current.classList.remove(styles.inputExpanded);
-		} else {
-			const inputEl = document.querySelector(`.${styles.searchInput}`);
-			if (inputEl) {
-				inputEl.classList.remove(styles.inputExpanded);
-			}
-		}
-		if (ref && ref.current) {
-			ref.current.blur(); // Quita el foco del input
-		}
-	};
-
-	// Close the input when the user scrolls
-	useEffect(() => {
-		const onScroll = (event) => {
-			if (window.scrollY > 100 && expandSearch) {
-				handleCloseSearch(event);
-			}
-		};
-		window.addEventListener('scroll', onScroll);
-		return () => {
-			window.removeEventListener('scroll', onScroll);
-		};
-	}, [expandSearch]);
-
-	// Close the input when the user clicks outside
-	useEffect(() => {
-		const handleClickOutside = (event) => {
-			if (
-				expandSearch && ref &&
-				ref.current &&
-				!ref.current.contains(event.target) &&
-				(!dropdownRef || !dropdownRef.current || !dropdownRef.current.contains(event.target))
-			) {
-				handleCloseSearch(event);
-			}
-		};
-		document.addEventListener('mousedown', handleClickOutside);
-		return () => {
-			document.removeEventListener('mousedown', handleClickOutside);
-		};
-	}, [expandSearch, ref, dropdownRef]);
-
-	// Close the input when the user changes the route
-	useEffect(() => {
-		const handleRouteChange = () => {
-			handleCloseSearch({ stopPropagation: () => {} });
-		};
-		router.events.on('routeChangeStart', handleRouteChange);
-		return () => {
-			router.events.off('routeChangeStart', handleRouteChange);
-		};
-	}, [router]);
+	// Custom Hook useInputSearch to controll the logic
+	const {
+		handleSearch,
+		handleKeyDown: hookHandleKeyDown,
+		handleCloseSearch
+	} = useInputSearch({
+		typesenseCollection,
+		handleKeyDown,
+		searchQuery,
+		setSearchQuery,
+		setResults,
+		setLoading,
+		expandSearch,
+		setExpandSearch,
+		onInputChange,
+		dropdownRef,
+		ref
+	});
 
 	return (
 		<div
@@ -124,7 +53,7 @@ const InputSearch = forwardRef(({
 				ref={ref} 
 				type="text"
 				onChange={handleSearch}
-				onKeyDown={handleKeyDown}
+				onKeyDown={hookHandleKeyDown || handleKeyDown}
 				value={searchQuery}
 				placeholder="Buscar"
 				className={styles.searchInput}
